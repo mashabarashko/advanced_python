3ad535683e4ef9bccc3e8419c9a4eb30162f30b0
Maryia Barashka <mashabarashko@gmail.com>
2018-09-13 12:46:37 +0300

    Sync processes with lock

---
+++ lock.py
@@ -0,0 +1,26 @@
+from multiprocessing import Lock
+from multiprocessing import Process
+
+
+def print_even(lock1, lock2):
+    for x in range(0, 101, 2):
+        lock2.acquire()
+        print(x)
+        lock1.release()
+
+
+def print_odd(lock1, lock2):
+    for x in range(1, 101, 2):
+        lock1.acquire()
+        print(x)
+        lock2.release()
+
+
+if __name__ == '__main__':
+    lock1 = Lock()
+    lock2 = Lock()
+    lock1.acquire()
+    t1 = Process(target=print_even, args=(lock1, lock2))
+    t2 = Process(target=print_odd, args=(lock1, lock2))
+    t1.start()
+    t2.start()
